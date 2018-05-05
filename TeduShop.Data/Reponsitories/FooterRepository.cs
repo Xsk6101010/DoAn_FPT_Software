cb856e5811db7cb3fdb73349e92e3ad1e74b8b2d
Hoàng An Tâm <hjhjcallme1@gmail.com>
2018-05-06 01:34:01 +0700

    add more repository

---
+++ TeduShop.Data/Reponsitories/FooterRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Models;
+
+namespace TeduShop.Data.Reponsitories
+{
+    public interface IFooterRepository
+    {
+    }
+
+    public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
+    {
+        public FooterRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+
+        }
+    }
+}
---
+++ TeduShop.Data/Reponsitories/PageRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Reponsitories
+{
+    public interface IPageRepository
+    {
+
+
+    }
+
+    public class PageRepository : RepositoryBase<Page>, IPageRepository
+    {
+        public PageRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
